005b1b02b6a93a0c8524139318d99747b82baba8
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-02-29 05:07:14 +0100

    Initial Commit

---
+++ Reel&Stories/Reel_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reel_StoriesApp.swift
+//  Reel&Stories
+//
+//  Created by Sidney MALEO on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Reel_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
